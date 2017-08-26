e66d03585180a51d6d42308d038927ba52c1c141
jrbeverly <jrbeverly@live.com>
2017-08-25 20:50:42 -0500

    (deps): Missing glfw-3.1.1 dep

---
+++ shared/glfw-3.1.1/build/examples/CMakeFiles/particles.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/jrbeverly/Downloads/cs488/shared/glfw-3.1.1/deps/getopt.c" "/home/jrbeverly/Downloads/cs488/shared/glfw-3.1.1/build/examples/CMakeFiles/particles.dir/__/deps/getopt.c.o"
+  "/home/jrbeverly/Downloads/cs488/shared/glfw-3.1.1/deps/tinycthread.c" "/home/jrbeverly/Downloads/cs488/shared/glfw-3.1.1/build/examples/CMakeFiles/particles.dir/__/deps/tinycthread.c.o"
+  "/home/jrbeverly/Downloads/cs488/shared/glfw-3.1.1/examples/particles.c" "/home/jrbeverly/Downloads/cs488/shared/glfw-3.1.1/build/examples/CMakeFiles/particles.dir/particles.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/jrbeverly/Downloads/cs488/shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "../deps"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/jrbeverly/Downloads/cs488/shared/glfw-3.1.1/src/context.c" "/home/jrbeverly/Downloads/cs488/shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/context.c.o"
+  "/home/jrbeverly/Downloads/cs488/shared/glfw-3.1.1/src/glx_context.c" "/home/jrbeverly/Downloads/cs488/shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/glx_context.c.o"
+  "/home/jrbeverly/Downloads/cs488/shared/glfw-3.1.1/src/init.c" "/home/jrbeverly/Downloads/cs488/shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/init.c.o"
+  "/home/jrbeverly/Downloads/cs488/shared/glfw-3.1.1/src/input.c" "/home/jrbeverly/Downloads/cs488/shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/input.c.o"
+  "/home/jrbeverly/Downloads/cs488/shared/glfw-3.1.1/src/linux_joystick.c" "/home/jrbeverly/Downloads/cs488/shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/linux_joystick.c.o"
+  "/home/jrbeverly/Downloads/cs488/shared/glfw-3.1.1/src/monitor.c" "/home/jrbeverly/Downloads/cs488/shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/monitor.c.o"
+  "/home/jrbeverly/Downloads/cs488/shared/glfw-3.1.1/src/posix_time.c" "/home/jrbeverly/Downloads/cs488/shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/posix_time.c.o"
+  "/home/jrbeverly/Downloads/cs488/shared/glfw-3.1.1/src/posix_tls.c" "/home/jrbeverly/Downloads/cs488/shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/posix_tls.c.o"
+  "/home/jrbeverly/Downloads/cs488/shared/glfw-3.1.1/src/window.c" "/home/jrbeverly/Downloads/cs488/shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/window.c.o"
+  "/home/jrbeverly/Downloads/cs488/shared/glfw-3.1.1/src/x11_init.c" "/home/jrbeverly/Downloads/cs488/shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/x11_init.c.o"
+  "/home/jrbeverly/Downloads/cs488/shared/glfw-3.1.1/src/x11_monitor.c" "/home/jrbeverly/Downloads/cs488/shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/x11_monitor.c.o"
+  "/home/jrbeverly/Downloads/cs488/shared/glfw-3.1.1/src/x11_window.c" "/home/jrbeverly/Downloads/cs488/shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/x11_window.c.o"
+  "/home/jrbeverly/Downloads/cs488/shared/glfw-3.1.1/src/xkb_unicode.c" "/home/jrbeverly/Downloads/cs488/shared/glfw-3.1.1/build/src/CMakeFiles/glfw.dir/xkb_unicode.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS
+  "_GLFW_USE_CONFIG_H"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src"
+  "src"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
